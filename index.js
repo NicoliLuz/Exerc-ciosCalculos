432e6bb721afa3bf18fb0c3cb884e62ff698d74f
NicoliLuz <azevedonicoligithub@gmail.com>
2024-03-05 11:30:29 -0300

    meu primeito commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// comando para exibir mensagem
+console.log("MEU ALERTA!")
+
+// exercício de calculo
+const num1 = 20;
+const num2 = 654;
+
+// soma
+let soma = num1 + num2
+
+// exibir o resultado da soma
+console.log("O valor da soma é " + soma);
+
+// subtração
+let subtração = num2 - num1;
+console.log("O valor da subtração é " + subtração);
+
+// multiplicação
+let multiplicação = num1 * num2;
+console.log("O valor da multiplicação é " + multiplicação);
+
+// divisão
+let divisão = num2 / num1;
+console.log("O valor da divisão é " + divisão);
